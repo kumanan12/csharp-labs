a0319e431dc4469e39de3b12bb1c2ef464efd6c8
meghaapunniya <meghaa2702@gmail.com>
2021-09-17 18:29:19 +0530

    updated

--- Meghaa-csharp/dataType/src/DataTypes/Program.cs
+++ Meghaa-csharp/dataType/src/DataTypes/Program.cs
@@ -33,9 +33,56 @@ namespace DataTypes
 
             int[] data = new int[3] {9,6,8};
             Console.WriteLine(data[0]);
+
+            Console.WriteLine("");
+            Console.WriteLine("adding int and string: ");
+            int first = 2;
+            string second = "4";
+            string Result = first + second;
+            Console.WriteLine(Result);
+
+            Console.WriteLine("");
+            Console.WriteLine("narrow conversion: ");
+            decimal myDecimal = 1.23456789m;
+            float myFloat = (float)myDecimal;
+
+            Console.WriteLine($"Decimal: {myDecimal}");
+            Console.WriteLine($"Float: {myFloat}");
         
-        
-        
+            Console.WriteLine("");
+            Console.WriteLine(" using the ToString() method to explicitly convert int values into strings: ");
+            int First = 5;
+            int Second = 7;
+            string message = First.ToString() + Second.ToString();
+            Console.WriteLine(message);
+
+            Console.WriteLine("");
+            Console.WriteLine("use the Parse() method to convert two strings into int values: ");
+            string firsT = "5";
+            string seconD = "7";
+            int sum = int.Parse(firsT) + int.Parse(seconD);
+            Console.WriteLine(sum);         
+
+            Console.WriteLine("");
+            Console.WriteLine("to compare casting and converting a decimal into an int: ");
+            int value = (int)1.5m; // casting truncates
+            Console.WriteLine(value);
+
+            int value2 = Convert.ToInt32(1.5m); // converting rounds up
+            Console.WriteLine(value2);
+
+            Console.WriteLine("");
+            Console.WriteLine("TryParse() a string into an int: ");
+            string Value = "102";
+            int result = 0;
+            if (int.TryParse(Value, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
         }
 
     }
