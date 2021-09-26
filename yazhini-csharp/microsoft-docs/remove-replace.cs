e6a9ad45e7667469624436427849783b23cd34f0
Yazhini-Krishnan <68785790+Yazhini-Krishnan@users.noreply.github.com>
2021-09-26 17:11:03 +0530

    ex-3-docs

---
+++ yazhini-csharp/microsoft-docs/remove-replace.cs
@@ -0,0 +1,4 @@
+string data = "12345Yazhini krishnan          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+
---
+++ yazhini-csharp/microsoft-docs/strings.cs
@@ -0,0 +1,23 @@
+//indexof() substringof()
+
+// string message = "JSON (javascript object notation)";
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
+
+// int length = closingPosition - openingPosition;
+
+// Console.WriteLine(message.Substring(openingPosition+1, length-1));
+
+// // Console.WriteLine(openingPosition);
+// // Console.WriteLine(closingPosition);
+
+
+// string message = "JSON <span>javascript object notation</span>?";
+
+// int openingPosition = message.IndexOf("<span>");
+// int closingPosition = message.IndexOf("</span>");
+
+
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition+6, length-6));
+
