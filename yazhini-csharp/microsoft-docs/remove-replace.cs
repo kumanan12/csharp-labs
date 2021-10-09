9c6cf4a8179c8f73fac2666675a8b488752b4577
Yazhini-Krishnan <68785790+Yazhini-Krishnan@users.noreply.github.com>
2021-10-09 16:34:17 +0530

    c#Fundamentals-exceptionHandeling

--- yazhini-csharp/microsoft-docs/remove-replace.cs
+++ yazhini-csharp/microsoft-docs/remove-replace.cs
@@ -1,4 +1,4 @@
-string data = "12345Yazhini krishnan          5000  3  ";
+string data = "12345Yazhini krishnan          12032001  ";
 string updatedData = data.Remove(5, 20);
 Console.WriteLine(updatedData);
 
