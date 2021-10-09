62d27376a981351ec3d1bdd8b5d1f26b9fb8cac3
Yazhini-Krishnan <68785790+Yazhini-Krishnan@users.noreply.github.com>
2021-10-09 17:21:14 +0530

    c#Syntax-loops

--- yazhini-csharp/gradebook/src/GradeBook/Program.cs
+++ yazhini-csharp/gradebook/src/GradeBook/Program.cs
@@ -6,13 +6,24 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            } 
-            else{
-                Console.WriteLine("Hello!");
-            }
+            var x = 12.1;
+            var y = 10.1;
+            var result = x+y;
+            Console.WriteLine(result);
+
+            var number = new double[3];
+            number[0] = 10;
+            number[1] = 12;
+            number[2] = 10;
+            var result1 = number[0] + number[1] + number[2];
+            Console.WriteLine(result1) 
+            // if(args.Length > 0)
+            // {
+            //     Console.WriteLine($"Hello {args[0]}!");
+            // } 
+            // else{
+            //     Console.WriteLine("Hello!");
+            // }
 
             
         }
